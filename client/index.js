793d0e03eaf5cae29ae3677239e65b0d59fa8a70
Alberto Esquivel <albertoesquivelanias@gmail.com>
2019-10-14 00:14:17 -0400

    added header

--- client/index.js
+++ client/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import App from './components/App';
+
+const client = new ApolloClient({
+    dataIdFromObject: o => o.id,
+});
 
 const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <Router history={browserHistory}>
+                <Route path='/' component={App} >
+                    
+                </Route>
+            </Router>
+        </ApolloProvider>
+    );
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,13 +1,15 @@
 const graphql = require('graphql');
 const {
     GraphQLObjectType,
+    GraphQLID,
     GraphQLString
 } = graphql;
 
 const UserType = new GraphQLObjectType({
     name: 'UserType',
     fields: {
-        email: { type: GraphQLString }
+        id: { type: GraphQLID },
+        email: { type: GraphQLString },
     }
 });
 
